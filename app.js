3b8c993292d515aba5d50d2aa724c9ae6cb12c09
aramrafeq <aramrafeq2@gmail.com>
2020-08-11 22:19:25 +0300

    bugfix

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const express = require('express')
-const cors = require('cors')
-const compression = require('compression')
-const helmet = require('helmet')
 const httpProxy = require('http-proxy');
 require('dotenv').config();
 
@@ -10,10 +7,6 @@ const apiProxy = httpProxy.createProxyServer({ proxyTimeout: 3000, timeout: 3000
 apiProxy.on('proxyReq', (proxyReq, req, res, options) => {
     // proxyReq.path += `&${cityQueryChunks}`;
 });
-
-app.use(compression());
-app.use(helmet());
-app.use(cors());
 
 app.use((req, res) => {
     var domain = req.get('host').match(/\w+/); // e.g., host: "subdomain.website.com"
