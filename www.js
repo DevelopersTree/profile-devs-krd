fce52cff44b28d9cd32aa31ef8de277587364e18
aramrafeq <aramrafeq2@gmail.com>
2020-08-12 11:13:13 +0300

    fixed

--- www.js
+++ www.js
@@ -1,18 +1,6 @@
-const https = require("https"),
-  fs = require("fs");
-
-const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/devs.krd/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/devs.krd/fullchain.pem")
-};
-
-
 const app  = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
 })
-
-
-https.createServer(options, app).listen(8080);
